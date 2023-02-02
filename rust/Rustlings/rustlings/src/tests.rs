e131f68ffaa7d9e8d645cec429a1b87f79a3a244
Grzegorz Osinski <grzegorz.osinski@accenture.com>
2023-02-02 14:31:48 +0100

    extra changes

--- rust/Rustlings/rustlings/src/tests.rs
+++ rust/Rustlings/rustlings/src/tests.rs
@@ -3,15 +3,19 @@
 //  - Signed Integer: i8, i16, i32, i64, isize, i128
 //  -  Float: f32, f64
 
-
-
 fn main() {
     return_value();
+    return_float();
 }
 
 // Lesson #26
 fn return_value()-> usize {
     24
+}
+
+// Lesson #27
+fn return_float()-> f32 {
+  24.5
 }
 
 #[cfg(test)]
@@ -22,28 +26,22 @@ mod tests {
     assert_eq!(return_value() as i32, 24);
   }
 }
-
-/*
-// Lesson #27
-fn main4() -> f32 {
-    24.5
-  }
-  
-  #[cfg(test)]
-  mod tests {
-    use crate::main;
+#[cfg(test)]
+  mod tests_float {
+    use crate::return_float;
     #[test]
     #[should_panic]
     fn main_panics_with_i() {
-      assert_eq!(main4() as usize as f32, main4() as f32);
+      assert_eq!(return_float() as usize as f32, return_float() as f32);
     }
     #[test]
     fn main_returns_f() {
-      assert_eq!(main4() as f32, 24.5);
+      assert_eq!(return_float() as f32, 24.5);
     }
   }
 
-  
+
+  /*
 // Lesson #28
 fn main5() -> f32 {
     24.5
