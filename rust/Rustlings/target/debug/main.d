cad77a0350daf4f8b010b9b07853256e57fe95e5
Grzegorz Osinski <grzegorz.osinski@accenture.com>
2023-04-20 11:43:36 +0200

    Updating gitignore

--- rust/Rustlings/target/debug/main.d
+++ rust/Rustlings/target/debug/main.d
@@ -1,1 +1,1 @@
-/mnt/c/Users/grzegorz.osinski/OneDrive\ -\ Accenture/Code/rust/Rustlings/target/debug/main: /mnt/c/Users/grzegorz.osinski/OneDrive\ -\ Accenture/Code/rust/Rustlings/rustlings/src/main.rs
+/mnt/c/code/rust/rust/Rustlings/target/debug/main: /mnt/c/code/rust/rust/Rustlings/rustlings/src/main.rs
